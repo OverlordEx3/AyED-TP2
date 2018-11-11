fe5b7b025a6309706579185d889179116486f9d6
roram <ces.ram.franco@gmail.com>
2018-11-10 22:26:53 -0300

    Add files via upload

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -1,16 +1,18 @@
-#include <stdio.h>
 #include <iostream>
-#include<conio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
 
 using namespace std;
 
-typedef char cadena[31];
+typedef char str30[31];
 
+//Creo la estructura del archivo
 struct TRBol{
 	
+	string nomEs;
 	int golFav;
-	cadena nomE;
+	str30 nomE;
 	
 };
 
@@ -20,12 +22,136 @@ struct NodoArbol{
 	NodoArbol *ptrDer;
 	struct TRBol valor;
 	
-}equipos[32];
+}VecPaisesGol[32];
 
+//Funcion para cargar equipos
 
+void cargarEquipos(){
+	
+	int salir = 0;
+	
+	//Instancio el archivo para poder utilizarlo y cargarle los datos
+	
+	TRBol RBol;
+	
+	//Con esto señalo el archivo con el que quiero trabajar
+	
+	FILE * FBol = fopen("bolillero.dat","wb+");;
 
+	//Ciclo para cargar los equipos y goles
+	
+	for(int i = 0; i < 32; i++){
+		
+		cout<<"Ingrese el nombre del equipo: ";
+		//CON EL GETLINE GUARDO LOS NOMBRES CON ESPACIOS
+		cin.getline(RBol.nomE,15);
+		cin.getline(RBol.nomE,15,'\n') ;
+		
+		cout<<"Ingrese Goles a Favor: ";
+		cin>>RBol.golFav;
+		cout<<"\n";
+			
+			
+		fwrite(&RBol,sizeof(RBol),1,FBol);
+		
+		
+	}
+	
+	fclose(FBol);
+	
+}
 
-void insertarNodo(NodoArbol* &ptrArbol, NodoArbol equipos[],int i){
+//Funcion para leer archivos
+
+void leerArchivo(){
+	
+	
+	//Instancio el archivo para trabajar desde el nombre del struct
+	
+	TRBol RBol;
+	
+	//Cargo el archivo con el que quiero trabajar
+	
+	FILE * FBol = fopen("bolillero.dat","rb");;
+	
+	//Este while imprime todos los datos del archivo :3
+	
+	fread(&RBol,sizeof(RBol),1,FBol);
+	
+	while(!feof(FBol)){
+		
+
+		
+		cout<<"Nombre del equipo: "<<RBol.nomE<<"\n";
+		
+			
+		cout<<"Goles a Favor: "<<RBol.golFav<<"\n\n";
+		
+		//Abro el archivo para poder leerlo
+		//ACORDATE QUE TIENE QUE IR AL FINAL, SINO TE LEE 2 VECES EL ARCHIVO AL FINAL :O
+		
+		fread(&RBol,sizeof(RBol),1,FBol);
+		
+	}
+	
+	fclose(FBol);
+	
+}
+
+void cargarVector(NodoArbol  VecPaisesGol[]){
+	
+	//Inicializo un valor para poder recorrer el array
+	int i = 0;
+	
+	//Instancio el archivo para trabajar desde el nombre del struct
+	
+	TRBol RBol;
+	
+	//Cargo el archivo con el que quiero trabajar
+	
+	FILE * FBol = fopen("bolillero.dat","rb");
+	
+	//Este while imprime todos los datos del archivo :3
+	
+	fread(&RBol,sizeof(RBol),1,FBol);
+	
+	while(!feof(FBol)){
+		
+		//Transformo los characters a tipo string, luego los asigno al array
+		string nomEs = RBol.nomE;
+		//Copio los valores del archivo al vector
+		
+		VecPaisesGol[i].valor.nomEs = nomEs;
+		//memcpy(VecPaisesGol[i].valor.nomEs, RBol.nomE, strlen(RBol.nomE));
+		
+		VecPaisesGol[i].valor.golFav = RBol.golFav;
+		
+		//Acutalizo el contador
+		
+		i = i + 1;
+		
+		fread(&RBol,sizeof(RBol),1,FBol);
+		
+	}
+	
+	fclose(FBol);
+	
+	
+}
+
+void leerVector(NodoArbol  VecPaisesGol[]){
+	
+	for(int i = 0; i < 32; i++){
+		
+		cout<<"Equipo: "<< VecPaisesGol[i].valor.nomEs<<" --- Goles: "<< VecPaisesGol[i].valor.golFav<<"\n";
+		
+		
+	}
+	
+}
+
+//Funcione para crear el Arbol
+void insertarNodo(NodoArbol* &ptrArbol, NodoArbol VecPaisesGol[],int i){
 	
 	
 	if(ptrArbol == NULL){
@@ -35,12 +161,16 @@ void insertarNodo(NodoArbol* &ptrArbol, NodoArbol equipos[],int i){
 		
 		//carga los valores al nodo creado
 		
-		aux -> valor.golFav  = equipos[i].valor.golFav;
+		aux -> valor.golFav  = VecPaisesGol[i].valor.golFav;
 		
-		aux -> valor.nomE  = equipos[i].valor.nomE;
+		//*************************************************************
+		//Problema por tipo de datos que le pasa D:
+		//*************************************************************
+		
+		//aux -> valor.nomE  = VecPaisesGol[i].valor.nomE;
 		
 		
-		//Pone los hijos en NULL
+		//Pone los hijos en NULL para que despues los asignen
 		
 		aux -> ptrIzq = NULL;
 		
@@ -51,16 +181,16 @@ void insertarNodo(NodoArbol* &ptrArbol, NodoArbol equipos[],int i){
 		
 	}else{
 		//Si es menor al nodo lo inserta en la izquierda
-		if(equipos[i].valor.golFav < ptrArbol -> valor.golFav){
-			insertarNodo( ptrArbol -> ptrIzq, equipos, i);
+		if(VecPaisesGol[i].valor.golFav < ptrArbol -> valor.golFav){
+			insertarNodo( ptrArbol -> ptrIzq, VecPaisesGol, i);
 		}else{
 			
 			//Si es mayor o igual lo inserta en la rama derecha
-			if( equipos[i].valor.golFav > ptrArbol -> valor.golFav ){	
-				insertarNodo(ptrArbol -> ptrDer, equipos, i);
+			if( VecPaisesGol[i].valor.golFav > ptrArbol -> valor.golFav ){	
+				insertarNodo(ptrArbol -> ptrDer, VecPaisesGol, i);
 			}else{
 				
-				//cout<< "\n" <<"Valor duplicado";
+				insertarNodo(ptrArbol -> ptrDer, VecPaisesGol, i);
 				
 			}
 			
@@ -70,8 +200,7 @@ void insertarNodo(NodoArbol* &ptrArbol, NodoArbol equipos[],int i){
 	
 }
 
-//Funcion para recorrido del arbol
-
+//Funcion para ordenar el arbol
 void inOrder(NodoArbol* &ptrArbol){
 	
 	if(ptrArbol == NULL){
@@ -89,130 +218,56 @@ void inOrder(NodoArbol* &ptrArbol){
 	
 }
 
-
-
-
 int main(int argc, char** argv) {
-	
-	
-	
-	equipos[0].valor.nomE = "Rusia";
-    equipos[0].valor.golFav = 11;
-    
-    equipos[1].valor.nomE = "Alemania";
-    equipos[1].valor.golFav = 2;
-    
-    equipos[2].valor.nomE = "Brasil";
-    equipos[2].valor.golFav = 8;
-    
-    equipos[3].valor.nomE = "Portugal";
-    equipos[3].valor.golFav = 6;
-    
-    equipos[5].valor.nomE = "Belgica";
-    equipos[5].valor.golFav = 16;
-    
-    equipos[6].valor.nomE = "Poloni";
-    equipos[6].valor.golFav = 2;
-	
-	equipos[7].valor.nomE = "Francia";
-    equipos[7].valor.golFav= 14;
-
-    equipos[8].valor.nomE = "España";
-    equipos[8].valor.golFav= 7;
-    
-    equipos[9].valor.nomE = "Peru";
-    equipos[9].valor.golFav= 2;
-    
-    equipos[10].valor.nomE = "Suiza";
-    equipos[10].valor.golFav= 5;
-    
-    equipos[11].valor.nomE = "Inglaterra";
-    equipos[11].valor.golFav= 12;
-    
-    equipos[12].valor.nomE = "Colombia";
-    equipos[12].valor.golFav= 6;
-    
-    equipos[13].valor.nomE = "Mexico";
-    equipos[13].valor.golFav= 3;
-    
-    equipos[14].valor.nomE = "Uruguay";
-    equipos[14].valor.golFav= 7;
-   
-    equipos[15].valor.nomE = "Croacia";
-    equipos[15].valor.golFav= 14;
-    
-    equipos[16].valor.nomE = "Dinamarca";
-    equipos[16].valor.golFav= 3;
-    
-    equipos[17].valor.nomE = "Islandia";
-    equipos[17].valor.golFav= 2;
-    
-    equipos[18].valor.nomE = "Costa Rica";
-    equipos[18].valor.golFav= 2;
-    
-    equipos[19].valor.nomE = "Suecia";
-    equipos[19].valor.golFav= 6;
-    
-    equipos[20].valor.nomE = "Tunez";
-    equipos[20].valor.golFav= 5;
-    
-    equipos[21].valor.nomE = "Egipto";
-    equipos[21].valor.golFav= 2;
-   
-    equipos[22].valor.nomE = "Senegal";
-    equipos[22].valor.golFav= 4;
-    
-    equipos[23].valor.nomE = "Iran";
-    equipos[23].valor.golFav= 2;
-    
-    equipos[24].valor.nomE = "Serbia";
-    equipos[24].valor.golFav= 2;
-    
-    equipos[25].valor.nomE = "Nigeria";
-    equipos[25].valor.golFav= 3;
-    
-    equipos[26].valor.nomE = "Australia";
-    equipos[26].valor.golFav= 2;
-    
-    equipos[27].valor.nomE = "Japon";
-    equipos[27].valor.golFav= 6;
-    
-    equipos[28].valor.nomE = "Marruecos";
-    equipos[28].valor.golFav= 2;
-    
-    equipos[29].valor.nomE = "Panama";
-    equipos[29].valor.golFav= 2;
-    
-    equipos[30].valor.nomE = "Corea del Sur";
-    equipos[30].valor.golFav= 3;
-    
-    equipos[31].valor.nomE = "Arabia Saudita";
-    equipos[31].valor.golFav= 2;
 	
 	NodoArbol* ptrRaiz = NULL;
 	
-
+	//Varialbe para elegir si quiero escribir archivo o solo leerlo
 	
-	/*
-	preOrden(ptrRaiz);
-	*/
-	for(int i = 0; i<32;i++ ){
+	
+	int opcion;
+	
+	cout<<"Bienvenido!!!\n";
+	
+	do{
 		
-	insertarNodo(ptrRaiz,equipos,i);		
+		cout<<"Ingrese 1 para cargar equipos: (No es necesario) "  <<"\n"<< "2 para leer los equipos cargados: "<<"\n"<< "3 cargar Vector: "<<"\n"<<"4 Leer vector: "<<"\n"<<"5 para ordernar y 0 para salir: ";
+		cin>>opcion;
+		switch(opcion)	{
+			case 0:{
+				cout<<"Final del programa";
+				break;
+			}
 		
-	}
-	
-	inOrder(ptrRaiz);
-	
-	//preOrden(ptrRaiz);	
-	/*
-	insertarNodo(ptrRaiz, 10);
-	insertarNodo(ptrRaiz, 4);
-	insertarNodo(ptrRaiz, 5);
-	insertarNodo(ptrRaiz, 9);
-	insertarNodo(ptrRaiz, 14);
-	*/
-	
+			case 1:{
+				cargarEquipos();
+				break;
+			}
+			case 2:{
+				leerArchivo();
+				break;
+			}
+			case 3:{
+				cargarVector(VecPaisesGol);
+				break;
+			}
+			case 4:{
+				leerVector(VecPaisesGol);
+				break;
+			}
+			case 5:{
+				for(int i = 0; i<32;i++ ){
+				insertarNodo(ptrRaiz,VecPaisesGol,i);		
+				}
+				inOrder(ptrRaiz);
+				break;
+			}
+			default:{
+				cout<<"Error, opcion invalida";
+				break;
+			}
+		}
+	}while(opcion != 0);
 
 	
 	return 0;
